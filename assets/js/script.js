1ed5b1e44b574341e8966cb34df5c80467fc8067
Shradha Kallaje <shradhakallaje@Shradhas-MacBook-Pro.local>
2023-11-26 21:47:15 +0100

    Adding code to shuffle the valid and bombs arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,10 @@ function createBoard() {
     const safeSquares = Array((width * width) - numberOfBombs).fill('valid');
     console.log(bombs);
     console.log(safeSquares);
+    let gameArray = safeSquares.concat(bombs);
+    console.log(gameArray);
+    let randomisedArray = shuffleArray(gameArray)
+    console.log(randomisedArray);
 
 
 
@@ -28,3 +32,20 @@ createBoard();
 
 
 })
+
+
+// Function to shuffle the gameArray content
+
+function shuffleArray(array) {
+    for (let i = 0; i < array.length; i++) {
+
+        // Generate random array index 
+        let j = Math.floor(Math.random() * (i + 1));
+        // Swapping each element in the array with a randon value
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
