051a26a25c9ced2f8289512e751a6663ad6aeb64
Shradha Kallaje <shradhakallaje@Shradhas-MacBook-Pro.local>
2023-11-26 22:29:22 +0100

    Add code to check for bombs in adjacent squares and update total of clicked square

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,54 @@ function createBoard() {
 
     const bombs = Array(numberOfBombs).fill('bomb');
     const safeSquares = Array((width * width) - numberOfBombs).fill('valid');
-    console.log(bombs);
-    console.log(safeSquares);
+
     let gameArray = safeSquares.concat(bombs);
-    console.log(gameArray);
+
     let randomisedArray = shuffleArray(gameArray)
-    console.log(randomisedArray);
+
 
 
 
     for (let i = 0; i < width * width; i++) {
         const square = document.createElement('div');
         square.setAttribute('id', i);
+        square.classList.add(randomisedArray[i]);
         grid.appendChild(square);
         squares.push(square);
+
+        // Click Event listener to handle clicking on one of the squares
+        square.addEventListener('click', function(event) {
+            click(square);
+        })
     }
+
+
+    // Add Numbers
+
+    for (let i = 0; i < squares.length; i++) {
+        let total = 0;
+        let isLeftEdge = (i % width === 0);
+        let isRightEdge = (i % width === width - 1);
+
+        if (squares[i].classList.contains('valid')) {
+            if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total ++;
+            if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++;
+            if (i > 10 && squares[i - width].classList.contains('bomb')) total ++;
+            if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total ++;
+            if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total ++;
+            if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++;
+            if (i < 88 && !isRightEdge && squares[i + width].classlist.contains('bomb')) total ++;
+            squares[i].setAttribute('data', total);
+            console.log(squares[i]);
+        }
+    }
+
+
+
+
+
+
+
 }
 createBoard();
 
@@ -48,4 +81,17 @@ function shuffleArray(array) {
     }
 
     return array;
+}
+
+
+// Click function 
+function click(square) {
+    if (square.classList.contains('bomb')) {
+        console.log('Game Over');
+    } else {
+        let total = square.getAttribute('data')
+        if (total !== 0) {
+            square.classList.add('checked');
+        }
+    }
 }
